e27af9e2c4f88509d9c02588202ba3028c226503
sfletche <sfletche@gmail.com>
2010-07-15 12:06:11 -0700

    adjusting labels/fields/titles/text from mpa-related to nearshore-habitat-related.  changed Create New text from mpa to nsh.   changed drawing panel title, removed designation field, and changed MPA field name to Habitat Name.  changed Marine Protected Areas folder under My Shapes to Nearshore Habitats.  changed Mpa model to Habitat, changed settings.MPA_Class to tsp.models.Habitat, changed MpaForm to HabitatForm and settings.MPA_FORM to tsp.forms.HabitatForm

--- omm/tsp/models.py
+++ omm/tsp/models.py
@@ -3,7 +3,9 @@ from django.db import models
 from lingcod.mpa.models import Mpa as BaseMpa
 from lingcod.array.models import MpaArray as BaseMpaArray
 
-class Mpa(BaseMpa):
+#if the names of the following two classes are changed, the related settings should also be changed (MPA_CLASS, ARRAY_CLASS)
+
+class Habitat(BaseMpa):
     pass
 
 class MpaArray(BaseMpaArray):
