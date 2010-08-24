2d54f743697d40817199d6cb378a990e38ee11a8
sfletche <sfletche@gmail.com>
2010-08-24 10:08:41 -0700

    moving some code into analysis/utils

--- omm/analysis/NSH_Analysis.py
+++ omm/analysis/NSH_Analysis.py
@@ -3,30 +3,8 @@ from NSH_Geography import display_geo_analysis
 from NSH_Physical import display_phy_analysis
 from NSH_Biology import display_bio_analysis
 from NSH_Human import display_hum_analysis
+from utils import type_is_geo, type_is_phy, type_is_bio, type_is_hum
 
-def type_is_geo(type):
-    lc_type = type.lower()
-    if lc_type in ['geo', 'geography']:
-        return True
-    return False
-    
-def type_is_phy(type):
-    lc_type = type.lower()
-    if lc_type in ['phy', 'physical']:
-        return True
-    return False
-    
-def type_is_bio(type):
-    lc_type = type.lower()
-    if lc_type in ['bio', 'biology']:
-        return True
-    return False
-    
-def type_is_hum(type):
-    lc_type = type.lower()
-    if lc_type in ['hum', 'human']:
-        return True
-    return False
 
 def display_nsh_analysis(request, nsh_id, type):
     if type_is_geo(type):
