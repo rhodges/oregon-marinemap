cf3ba0e0fa48462e311b1881454ce3dc5f558b25
sfletche <sfletche@gmail.com>
2010-08-27 10:51:34 -0700

    minor code cleaning

--- omm/analysis/NSH_Geography.py
+++ omm/analysis/NSH_Geography.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
-from tsp.models import AOI
 from models import *
 from settings import *
 from lingcod.unit_converter.models import area_in_display_units, length_in_display_units
