a72a37410d933fe0b3945010ac3429c57700bc13
sfletche <sfletche@gmail.com>
2010-09-07 14:40:40 -0700

    adding url for fishing map legend

--- omm/fishing_layers/urls.py
+++ omm/fishing_layers/urls.py
@@ -4,7 +4,7 @@ urlpatterns = patterns('fishing_layers.views',
     url(r'^public/$', 'get_fishing_layers', name='public-fishing-data-layers'),
     url(r'^public/tiles/map/([A-Za-z]+)/$', 'get_map'),
     url(r'^(\w+)/(\w+)/ecotrust/tiles/contour/([A-Za-z]+)/([A-Za-z0-9_,]+)/$', 'get_contour'),
-    url(r'^(\w+)/(\w+)/ecotrust/other/([A-Za-z]+)/([A-Za-z0-9_,-\.]+)/$', 'get_other'),
+    url(r'^public/other/([A-Za-z]+)/([A-Za-z0-9_,-\.]+)/$', 'get_other'),
     #note on the following 2 url patterns:
     #providing for an optional trailing slash ('/') to prevent GE big red X's generated from missed url attempts (301's)
     url(r'^(\w+)/(\w+)/ecotrust/tiles/map/([A-Za-z]+)/([A-Za-z0-9_,]+)/([0-9]+)/([0-9]+)/([0-9]+)\.([A-Za-z]+)/?$', 'get_map'),
