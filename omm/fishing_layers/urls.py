4e7136163422308e25f79df2523613612bbc82ec
sfletche <sfletche@gmail.com>
2010-09-09 13:39:19 -0700

    adding images for fishing ground description text

--- omm/fishing_layers/urls.py
+++ omm/fishing_layers/urls.py
@@ -4,7 +4,7 @@ urlpatterns = patterns('fishing_layers.views',
     url(r'^public/$', 'get_public_fishing_layers', name='public-fishing-data-layers'),
     url(r'^public/tiles/map/([A-Za-z]+)/$', 'get_public_map'),
     url(r'^public/tiles/contour/([A-Za-z]+)/$', 'get_public_contour'),
-    url(r'^public/other/([A-Za-z]+)/([A-Za-z0-9_,-\.]+)/$', 'get_public_other'),
+    url(r'^public/other/([A-Za-z]+)/([A-Za-z0-9_-]+).([A-Za-z]+)/$', 'get_public_other'),
     #note on the following 2 url patterns:
     #providing for an optional trailing slash ('/') to prevent GE big red X's generated from missed url attempts (301's)
     url(r'^public/tiles/map/([A-Za-z]+)/([0-9]+)/([0-9]+)/([0-9]+)\.([A-Za-z]+)/?$', 'get_public_map'),
