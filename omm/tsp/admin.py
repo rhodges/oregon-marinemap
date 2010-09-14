e6b2dc18046e413a75c4c039345052e50a194bfd
sfletche <sfletche@gmail.com>
2010-09-14 14:23:19 -0700

    adding aoi to admin

--- omm/tsp/admin.py
+++ omm/tsp/admin.py
@@ -2,3 +2,11 @@ from django.contrib import admin
 
 from django.contrib.auth.models import Permission
 admin.site.register(Permission)
+
+from omm.tsp.models import AOI
+
+class AOIAdmin(admin.ModelAdmin):
+    list_display = ( 'pk', 'name', 'user', 'date_created', 'date_modified')
+    search_fields = ('name','id','user__username','user__first_name','user__last_name')
+    
+admin.site.register(AOI, AOIAdmin)
