674edf7525792884e8884179cb03b29ead447484
sfletche <sfletche@gmail.com>
2010-09-17 12:13:14 -0700

    adding some changes related to pdf reports...still have some tweaking to do, hiding the links for now.  this update requires update of marinemap.lingcod.staticmap as well as installation of pisa, reportlab and html5lib, see requirements.txt for version numbers.

--- omm/analysis/NSH_Analysis.py
+++ omm/analysis/NSH_Analysis.py
@@ -38,25 +38,64 @@ def run_nsh_analysis(nsh, type):
     
 '''
 renders template with type-specific context
-called by views.print_report
+called by views.print_nsh_report
 '''    
-def print_nsh_report(request, nsh, type, template=None):
-    if type_is_geo(type):
-        template='printable_nsh_geo_report.html'
-    elif type_is_phy(type):
-        template='printable_nsh_phy_report.html'
-    elif type_is_bio(type):
-        template='printable_nsh_bio_report.html'
-    elif type_is_hum(type):
-        template='printable_nsh_hum_report.html'
-    else:
-        template='printable_nsh_comprehensive_report.html'    
+def printable_report(request, nsh, type):
+    template = get_printable_template(type)
     context = get_or_create_cache(nsh, type)
     return render_to_response(template, RequestContext(request, context))
     
 '''
+renders printable template as pdf
+called by views.pdf_nsh_report
+'''    
+def pdf_report(request, nsh, type):
+    from django.template.loader import render_to_string
+    import ho.pisa as pisa 
+    import cStringIO as StringIO
+    template = get_printable_template(type)
+    context = get_or_create_cache(nsh, type)
+    html = render_to_string(template, context)
+    result = StringIO.StringIO()
+    pdf = pisa.pisaDocument(StringIO.StringIO(html.encode("UTF-8")), dest=result, link_callback=fetch_resources)
+    if not pdf.err:
+        return HttpResponse(result.getvalue(), mimetype='application/pdf')
+    return HttpResponse('This PDF request is temporarily un-available.') 
+    
+'''
+Returns a path to desired resource (image file)
+Called from within pisaDocument via link_callback parameter
+'''    
+def fetch_resources(uri, rel):
+    import os
+    import settings
+    from lingcod.staticmap.views import save_to_temp
+    params = get_params_from_uri(uri)
+    if len(params) > 0:
+        path = save_to_temp(params)
+    else:
+        path = os.path.join(settings.MEDIA_ROOT, uri.replace(settings.MEDIA_URL, ""))
+    return path
+    
+'''
+Returns a dictionary representation of the parameters attached to the given uri
+Called by fetch_resources
+'''    
+def get_params_from_uri(uri):
+    from urlparse import urlparse
+    results = urlparse(uri)
+    params = {}
+    if results.query == '':
+        return params
+    params_list = results.query.split('&')
+    for param in params_list:
+        pair = param.split('=')
+        params[pair[0]] = pair[1]
+    return params
+    
+'''
 retrieves the type-specific context from cache, or if not yet cached, by running the analysis
-called by print_nsh_report
+called by printable_report and pdf_report
 '''      
 def get_or_create_cache(nsh, type):
     if type == 'all':
@@ -74,3 +113,20 @@ def get_or_create_cache(nsh, type):
         context = run_nsh_analysis(nsh, type)
     return context
         
+'''
+Returns template name associated with given type
+Called by printable_report and pdf_report
+'''        
+def get_printable_template(type):
+    if type_is_geo(type):
+        template='printable_nsh_geo_report.html'
+    elif type_is_phy(type):
+        template='printable_nsh_phy_report.html'
+    elif type_is_bio(type):
+        template='printable_nsh_bio_report.html'
+    elif type_is_hum(type):
+        template='printable_nsh_hum_report.html'
+    else:
+        template='printable_nsh_comprehensive_report.html'    
+    return template
+    
--- omm/analysis/urls.py
+++ omm/analysis/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     url(r'aes/(\d+)/(\w+)', aes_analysis, name='aes_analysis'),
     #user requested printable reports
     url(r'nsh/print_report/(\d+)/(\w+)', print_nsh_report, name='printable_nsh_report'),
+    url(r'nsh/pdf_report/(\d+)/(\w+)', pdf_nsh_report, name='pdf_nsh_report'),
     #admin request cache events
     url(r'adminClearNShCache/(\w+)/', adminClearNSHCache),
 )  
