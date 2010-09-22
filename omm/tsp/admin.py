8faa5a8b759d8f5ec2d12c7329ca4f11fe6c78d9
sfletche <sfletche@gmail.com>
2010-09-22 10:42:56 -0700

    adding aoi arrays to admin

--- omm/tsp/admin.py
+++ omm/tsp/admin.py
@@ -3,10 +3,19 @@ from django.contrib import admin
 from django.contrib.auth.models import Permission
 admin.site.register(Permission)
 
-from omm.tsp.models import AOI
+from omm.tsp.models import AOI, AOIArray
 
 class AOIAdmin(admin.ModelAdmin):
     list_display = ( 'pk', 'name', 'user', 'date_created', 'date_modified')
+    list_filter = ['date_modified','date_created']
     search_fields = ('name','id','user__username','user__first_name','user__last_name')
     
 admin.site.register(AOI, AOIAdmin)
+
+class AOIArrayAdmin(admin.ModelAdmin):
+    list_display = ( 'pk', 'name', 'user', 'date_created', 'date_modified')
+    list_filter = ['date_modified','date_created']
+    search_fields = ('name','id','user__username','user__first_name','user__last_name')
+    
+admin.site.register(AOIArray, AOIArrayAdmin)
+
