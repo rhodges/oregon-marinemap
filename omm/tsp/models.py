4757e3157fababb8dce32ba36fa0bc268dd50559
sfletche <sfletche@gmail.com>
2010-10-22 10:53:15 -0700

    minor changes in attempt to get optional manipulators working on omm

--- omm/tsp/models.py
+++ omm/tsp/models.py
@@ -3,11 +3,16 @@ from django.contrib.gis.db import models
 
 from lingcod.mpa.models import Mpa as BaseMpa
 from lingcod.array.models import MpaArray as BaseMpaArray
+from lingcod.manipulators.manipulators import ClipToStudyRegionManipulator
 
 #if the names of the following two classes are changed, the related settings should also be changed (MPA_CLASS, ARRAY_CLASS)
 
 class AOI(BaseMpa):
     description = models.TextField(default="", null=True, blank=True)
+    
+    class Options:
+        manipulators = []
+        optional_manipulators = [ ClipToStudyRegionManipulator, ]
 
 class AOIArray(BaseMpaArray):
     pass
