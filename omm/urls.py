43387586724338e629c2f4b4e517d7bfb7e38784
sfletche <sfletche@gmail.com>
2010-11-15 12:38:17 -0800

    adding new app for non-consumptive data layer display.  kmz and legend images have been loaded onto cloudfront.  doc.kml needs to be loaded onto dev and prod via admin.

--- omm/urls.py
+++ omm/urls.py
@@ -29,6 +29,7 @@ urlpatterns = patterns('',
     (r'^analysis/', include('analysis.urls')),
     (r'^fishing/', include('fishing_layers.urls')),
     (r'^loadshp/', include('lingcod.loadshp.urls')),
+    (r'^non_consumptive/', include('non_consumptive.urls')),
 
 )
 
