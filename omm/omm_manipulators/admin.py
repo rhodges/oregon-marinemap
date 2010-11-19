ea1145355ed44c2f3ef81336da96ba3c1e943e6d
sfletche <sfletche@gmail.com>
2010-11-19 14:38:34 -0800

    removing the now unnecessary save method from the manipulator model classes.  adding remaining model classes to the admin.

--- omm/omm_manipulators/admin.py
+++ omm/omm_manipulators/admin.py
@@ -1,8 +1,23 @@
 from django.contrib.gis import admin
-from omm.omm_manipulators.models import EastOfTerritorialSeaLine
+from omm.omm_manipulators.models import *
 
 
 class EastOfTerritorialSeaLineAdmin(admin.ModelAdmin):
     list_display = ('name', 'active', 'creation_date',)
     
 admin.site.register(EastOfTerritorialSeaLine, EastOfTerritorialSeaLineAdmin)
+
+class TerrestrialAndEstuariesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'active', 'creation_date',)
+    
+admin.site.register(TerrestrialAndEstuaries, TerrestrialAndEstuariesAdmin)
+
+class TerrestrialAdmin(admin.ModelAdmin):
+    list_display = ('name', 'active', 'creation_date',)
+    
+admin.site.register(Terrestrial, TerrestrialAdmin)
+
+class EstuariesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'active', 'creation_date',)
+    
+admin.site.register(Estuaries, EstuariesAdmin)
