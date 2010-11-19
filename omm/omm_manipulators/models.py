66499e4764f2d15ec8572a3624bf04f0510fe1af
sfletche <sfletche@gmail.com>
2010-11-19 14:38:34 -0800

    removing the now unnecessary save method from the manipulator model classes.  adding remaining model classes to the admin.

--- omm/omm_manipulators/models.py
+++ omm/omm_manipulators/models.py
@@ -10,49 +10,25 @@ class EastOfTerritorialSeaLine(BaseManipulatorGeometry):
     
     def __unicode__(self):
         return "EastOfTerritorialSeaLine Layer, created: %s" % (self.creation_date)
-    
-    def save(self, *args, **kwargs):
-        super(EastOfTerritorialSeaLine, self).save(*args, **kwargs)
-        if self.active and EastOfTerritorialSeaLine.objects.filter(active=True).count() > 1:
-            # Ensure that any previously active layer is deactivated -- There can be only one!
-            EastOfTerritorialSeaLine.objects.filter(active=True).exclude(pk=self.pk).update(active=False)
-            
+        
 class TerrestrialAndEstuaries(BaseManipulatorGeometry):
     name = models.CharField(verbose_name="Terrestrial and Estuaries Geometry Name", max_length=255, blank=True)
     geometry = models.MultiPolygonField(srid=settings.GEOMETRY_DB_SRID, null=True, blank=True, verbose_name="Terrestrial-and-Estuaries")
 
     def __unicode__(self):
         return "Terrestrial and Estuaries Layer, created: %s" % (self.creation_date)
-    
-    def save(self, *args, **kwargs):
-        super(TerrestrialAndEstuaries, self).save(*args, **kwargs)
-        if self.active and TerrestrialAndEstuaries.objects.filter(active=True).count() > 1:
-            # Ensure that any previously active layer is deactivated -- There can be only one!
-            TerrestrialAndEstuaries.objects.filter(active=True).exclude(pk=self.pk).update(active=False)
-
+        
 class Terrestrial(BaseManipulatorGeometry):
     name = models.CharField(verbose_name="Terrestrial Geometry Name", max_length=255, blank=True)
     geometry = models.MultiPolygonField(srid=settings.GEOMETRY_DB_SRID, null=True, blank=True, verbose_name="Terrestrial-Only")
 
     def __unicode__(self):
         return "Terrestrial Layer, created: %s" % (self.creation_date)
-    
-    def save(self, *args, **kwargs):
-        super(Terrestrial, self).save(*args, **kwargs)
-        if self.active and Terrestrial.objects.filter(active=True).count() > 1:
-            # Ensure that any previously active layer is deactivated -- There can be only one!
-            Terrestrial.objects.filter(active=True).exclude(pk=self.pk).update(active=False)
-
+        
 class Estuaries(BaseManipulatorGeometry):
     name = models.CharField(verbose_name="Estuaries Geometry Name", max_length=255, blank=True)
     geometry = models.MultiPolygonField(srid=settings.GEOMETRY_DB_SRID, null=True, blank=True, verbose_name="Estuaries-Only")
 
     def __unicode__(self):
         return "Estuaries Layer, created: %s" % (self.creation_date)
-    
-    def save(self, *args, **kwargs):
-        super(Estuaries, self).save(*args, **kwargs)
-        if self.active and Estuaries.objects.filter(active=True).count() > 1:
-            # Ensure that any previously active layer is deactivated -- There can be only one!
-            Estuaries.objects.filter(active=True).exclude(pk=self.pk).update(active=False)
-
+        
