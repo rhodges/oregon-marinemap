49def38b1d0b31b120afdfdd47108b7c0ddba151
sfletche <sfletche@gmail.com>
2010-11-18 18:12:28 -0800

    adding models, manipulators, migrations, templates, and fixtures for additional manipulators, ExcludeTerrestrial and ExcludeStateWaters.  waiting on additional data layer for estuaries manipulator.  might migrate DifferenceFromShapeManipulator to lingcod later.  also there is the oddity of needing the really_small_area variable for dealing with results that should theoretically be empty but are not empty in actuality

--- omm/omm_manipulators/models.py
+++ omm/omm_manipulators/models.py
@@ -4,13 +4,13 @@ from django.conf import settings
 from lingcod.manipulators.manipulators import BaseManipulator, ClipToShapeManipulator
 
 
-class EastOfTerritorialSeaLineManager(models.GeoManager):
+class BaseManipulatorGeometryManager(models.GeoManager):
     """Returns the currently active data layer (determined by the active attribute).
     """
     def current(self):
         return self.get(active=True)
 
-class EastOfTerritorialSeaLine(models.Model):
+class BaseManipulatorGeometry(models.Model):
     """Model used for storing data layer related to ExcludingFederalWatersManipulator
 
         ======================  ==============================================
@@ -23,12 +23,8 @@ class EastOfTerritorialSeaLine(models.Model):
                                 data layer. If set to true and and another
                                 layer is active, that old layer will be 
                                 deactivated.
-
-        ``geometry``            PolygonField representing the non-federal-waters
         ======================  ==============================================
     """    
-    name = models.CharField(verbose_name="East Of Territorial Sea Line Name", max_length=255, blank=True)
-    
     creation_date = models.DateTimeField(auto_now=True) 
     
     active = models.BooleanField(default=True, help_text="""
@@ -37,9 +33,15 @@ class EastOfTerritorialSeaLine(models.Model):
         system so you can go back to an old version if necessary.
     """)
     
-    geometry = models.MultiPolygonField(srid=settings.GEOMETRY_DB_SRID, null=True, blank=True, verbose_name="Non-Federal-Waters")
+    objects = BaseManipulatorGeometryManager()
     
-    objects = EastOfTerritorialSeaLineManager()
+    class Meta:
+        abstract = True
+    
+
+class EastOfTerritorialSeaLine(BaseManipulatorGeometry):
+    name = models.CharField(verbose_name="East Of Territorial Sea Line Name", max_length=255, blank=True)
+    geometry = models.MultiPolygonField(srid=settings.GEOMETRY_DB_SRID, null=True, blank=True, verbose_name="Non-Federal-Waters")
     
     def __unicode__(self):
         return "EastOfTerritorialSeaLine Layer, created: %s" % (self.creation_date)
@@ -50,3 +52,46 @@ class EastOfTerritorialSeaLine(models.Model):
             # Ensure that any previously active layer is deactivated
             # There can be only one!
             EastOfTerritorialSeaLine.objects.filter(active=True).exclude(pk=self.pk).update(active=False)
+            
+class TerrestrialAndEstuaries(BaseManipulatorGeometry):
+    name = models.CharField(verbose_name="Terrestrial and Estuaries Geometry Name", max_length=255, blank=True)
+    geometry = models.MultiPolygonField(srid=settings.GEOMETRY_DB_SRID, null=True, blank=True, verbose_name="Terrestrial-and-Estuaries")
+
+    def __unicode__(self):
+        return "Terrestrial and Estuaries Layer, created: %s" % (self.creation_date)
+    
+    def save(self, *args, **kwargs):
+        super(TerrestrialAndEstuaries, self).save(*args, **kwargs)
+        if self.active and TerrestrialAndEstuaries.objects.filter(active=True).count() > 1:
+            # Ensure that any previously active layer is deactivated
+            # There can be only one!
+            TerrestrialAndEstuaries.objects.filter(active=True).exclude(pk=self.pk).update(active=False)
+
+class Terrestrial(BaseManipulatorGeometry):
+    name = models.CharField(verbose_name="Terrestrial Geometry Name", max_length=255, blank=True)
+    geometry = models.MultiPolygonField(srid=settings.GEOMETRY_DB_SRID, null=True, blank=True, verbose_name="Terrestrial-Only")
+
+    def __unicode__(self):
+        return "Terrestrial Layer, created: %s" % (self.creation_date)
+    
+    def save(self, *args, **kwargs):
+        super(Terrestrial, self).save(*args, **kwargs)
+        if self.active and Terrestrial.objects.filter(active=True).count() > 1:
+            # Ensure that any previously active layer is deactivated
+            # There can be only one!
+            Terrestrial.objects.filter(active=True).exclude(pk=self.pk).update(active=False)
+
+class Estuaries(BaseManipulatorGeometry):
+    name = models.CharField(verbose_name="Estuaries Geometry Name", max_length=255, blank=True)
+    geometry = models.MultiPolygonField(srid=settings.GEOMETRY_DB_SRID, null=True, blank=True, verbose_name="Estuaries-Only")
+
+    def __unicode__(self):
+        return "Estuaries Layer, created: %s" % (self.creation_date)
+    
+    def save(self, *args, **kwargs):
+        super(Estuaries, self).save(*args, **kwargs)
+        if self.active and Estuaries.objects.filter(active=True).count() > 1:
+            # Ensure that any previously active layer is deactivated
+            # There can be only one!
+            Estuaries.objects.filter(active=True).exclude(pk=self.pk).update(active=False)
+
