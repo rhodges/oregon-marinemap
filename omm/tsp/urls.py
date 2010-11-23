e8be3e28cf8971ab876932628b9f57df149d6ee1
sfletche <sfletche@gmail.com>
2010-11-23 15:17:17 -0800

    adding export aoi as shapefile functionality.  includes new model to tsp, new properties to AOI model, new url, and new view to tsp, as well as related migrations for new tsp model

--- omm/tsp/urls.py
+++ omm/tsp/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 from lingcod.common.utils import get_mpa_form, get_array_form
+from views import *
 
 #the following url pattern was acquired from lingcod.mpa.urls
 
@@ -9,4 +10,5 @@ ArrayForm = get_array_form()
 urlpatterns = patterns('lingcod.rest.views',
     url(r'^form/aoi/$', 'form_resources', {'form_class': AOIForm, 'create_title': 'Create a New Area of Inquiry'}, name='aoi_create_form'),
     url(r'^form/array/$', 'form_resources', {'form_class': ArrayForm, 'create_title': 'Create a New AOI Group'}, name='array_create_form'),
+    url(r'shapefile/mpa/(?P<mpa_id_list_str>(\d+,?)+)/$', aoi_shapefile, name='mpa_shapefile'),
 )
