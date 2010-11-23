db383a5c34a9c5bf7c1455829ad097c8d77d7299
sfletche <sfletche@gmail.com>
2010-11-23 15:17:17 -0800

    adding export aoi as shapefile functionality.  includes new model to tsp, new properties to AOI model, new url, and new view to tsp, as well as related migrations for new tsp model

--- omm/tsp/views.py
+++ omm/tsp/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from lingcod.common import utils
+from lingcod.sharing.utils import get_viewable_object_or_respond
+from lingcod.shapes.views import ShpResponder
+from django.template.defaultfilters import slugify
+
+def aoi_shapefile(request, mpa_id_list_str):
+    aoi_class = utils.get_mpa_class()
+    aoi_id_list = mpa_id_list_str.split(',')
+    aoi_id = aoi_id_list[0] # only expecting one for now
+    aoi = get_viewable_object_or_respond(aoi_class, aoi_id, request.user)
+    shp_response = ShpResponder(aoi.export_query_set)
+    shp_response.file_name = slugify(aoi.name[0:8])
+    return shp_response()
