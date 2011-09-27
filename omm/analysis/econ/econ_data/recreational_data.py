1d43bd50410df19016d97501c02cc1c96f35304c
sfletche <sfletche@gmail.com>
2011-09-27 15:43:11 -0700

    adding percentage column to econ reports

---
+++ omm/analysis/econ/econ_data/recreational_data.py
@@ -0,0 +1,50 @@
+
+port_names = ['Astoria', 'Garibaldi/Tillamook', 'Salmon River', 'Depoe Bay', 'Newport', 'Florence', 'SOORC Ports', 'Gold Beach & Brookings']
+port_abbrs = {  'Astoria': 'astr', 
+                'Garibaldi/Tillamook': 'tllr', 
+                'Salmon River': 'slmr', 
+                'Depoe Bay': 'dpor', 
+                'Newport': 'newr', 
+                'Florence': 'flrr', 
+                'SOORC Ports': 'srcr', 
+                'Gold Beach & Brookings': 'bgdr' }
+
+fisheries = [   #('Dungeness crab - trap', 'dcrab'),
+                #('Flatfish - trap', 'flat'), 
+                ('Pacific Halibut - longline', 'phal'),
+                ('Rockfish - hook & line (dead)', 'rckf'),
+                ('Salmon - troll', 'sal') ]                     
+
+multipliers = { 'dcrab': 1., 
+                'flat': 1., 
+                'phal': 1., 
+                'rckf': 1., 
+                'sal': 1.  } 
+                
+rec_totals = {  'astr':{'rckf': 8366.23, 
+                        'phal': 2218.81, 
+                        'sal' : 424370.3 },                  
+                'dpor':{'rckf': 62960.00, 
+                        'phal': 15202.03, 
+                        'sal' : 138881.11 },                  
+                'flrr':{'rckf': 404.27, 
+                        'phal': 4566.23, 
+                        'sal' : 69034.01 },                  
+                'tllr':{'rckf': 75446.46, 
+                        'phal': 49735.99, 
+                        'sal' : 404925.52 },                 
+                'bgdr':{'rckf': 827634.74,
+                        'sal' : 333832.26 },                 
+                'newr':{'rckf': 170773.73, 
+                        'phal': 270325.95, 
+                        'sal' : 383583.05 },                 
+                'slmr':{'rckf': 123421.52, 
+                        'phal': 10029.89, 
+                        'sal' : 102155.48 },                 
+                'srcr':{'rckf': 261089.96, 
+                        'phal': 36929.97, 
+                        'sal' : 868692.54 },
+                'state':{'rckf':1530096.91,
+                         'phal':389008.87,
+                         'sal' :2725474.26 }
+            }
--- omm/analysis/econ/recreational.py
+++ omm/analysis/econ/recreational.py
@@ -1,29 +1,9 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from utils import get_rasterstats, raster_exists, get_keys, update_ports, update_state_totals, get_value
+from econ_data.recreational_data import port_names, port_abbrs, fisheries, multipliers, rec_totals
 
 default_value = '---'
-port_names = ['Astoria', 'Garibaldi/Tillamook', 'Salmon River', 'Depoe Bay', 'Newport', 'Florence', 'SOORC Ports', 'Gold Beach & Brookings']
-port_abbrs = {  'Astoria': 'astr', 
-                'Garibaldi/Tillamook': 'tllr', 
-                'Salmon River': 'slmr', 
-                'Depoe Bay': 'dpor', 
-                'Newport': 'newr', 
-                'Florence': 'flrr', 
-                'SOORC Ports': 'srcr', 
-                'Gold Beach & Brookings': 'bgdr' }
-
-fisheries = [   #('Dungeness crab - trap', 'dcrab'),
-                #('Flatfish - trap', 'flat'), 
-                ('Pacific Halibut - longline', 'phal'),
-                ('Rockfish - hook & line (dead)', 'rckf'),
-                ('Salmon - troll', 'sal') ]                     
-
-multipliers = { 'dcrab': 1., 
-                'flat': 1., 
-                'phal': 1., 
-                'rckf': 1., 
-                'sal': 1.  } 
  
 '''
 called by views.shoreside_analysis
@@ -43,7 +23,9 @@ def get_recreational_context(aoi, type='rec', prefix='rec'):
             multiplier = multipliers[fishery_abbr]
             if raster_exists(raster_name):
                 gross_revenue, total = get_rasterstats(aoi, raster_name, multiplier)
-                fish_list = [fishery, multiplier, gross_revenue, total]
+                fishery_total = rec_totals[port_abbr][fishery_abbr]
+                percentage = gross_revenue / fishery_total
+                fish_list = [fishery, multiplier, gross_revenue, total, percentage]
                 if port_name in get_keys(ports):
                     update_ports(ports, port_name, fish_list)
                 else:
@@ -53,7 +35,20 @@ def get_recreational_context(aoi, type='rec', prefix='rec'):
                 else:
                     state_totals.append((fishery, [multiplier, gross_revenue, total]))
         state_totals.sort()
+    add_state_percentages(state_totals)
     context = {type: {'aoi': aoi, 'default_value': default_value, 'ports': ports, 'state_totals': state_totals, 'pdf': False, 'printable': False}}
     create_econ_cache(aoi, type, context)
     return context
     
+def add_state_percentages(state_totals):
+    for tuple in state_totals:
+        fishery_abbr = get_fishery_abbr(tuple[0])
+        statewide_total = rec_totals['state'][fishery_abbr]
+        percentage = tuple[1][1] / statewide_total
+        tuple[1].append(percentage)
+    
+def get_fishery_abbr(fishery_name):
+    for fishery in fisheries:
+        if fishery[0] == fishery_name:
+            return fishery[1]
+    raise ValueError('incorrect value, %s, sent to get_fishery_abbr in recreational.py' % fishery_name)
